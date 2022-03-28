4b478d9e4978fecc6e77298079f10422c7dde528
Rodrigo “Albo” Arce <albo@pragmore.com>
2022-03-28 00:48:30 -0300

    Add login

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,6 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
         'email',
         'password',
     ];
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,13 +12,6 @@
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
-
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
-
             <!-- Email Address -->
             <div class="mt-4">
                 <x-label for="email" :value="__('Email')" />
