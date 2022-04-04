999f506fd931ee7bc9d96011f9a3da82a059cecb
Rodrigo “Albo” Arce <albo@pragmore.com>
2022-04-04 14:08:05 -0300

    Add redirection

---
+++ database/migrations/2022_04_04_164727_create_domains_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('domains', function (Blueprint $table) {
+            $table->id();
+            $table->string('fqdn');
+            $table->string('redirect');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('domains');
+    }
+};
