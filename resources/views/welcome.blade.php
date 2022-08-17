aeb8f3aa196343827f533c87b89278128a717172
Rodrigo “Albo” Arce <albo@pragmore.com>
2022-08-17 00:15:02 -0300

    Update Blouse

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20viewBox=%220%200%20100%20100%22%3E%3Ctext%20y=%22.9em%22%20font-size=%2290%22%3E👩‍💻%3C/text%3E%3C/svg%3E">
-  <link rel="stylesheet" href="https://cdn.pragmore.com/2.0.4/solid/blouse.css?font=Poppins&family=sans-serif">
+  <link rel="stylesheet" href="https://cdn.pragmore.com/2.0.5/solid/blouse.css?font=Poppins&family=sans-serif">
   <style>body:after{content:"BETA";position:fixed;width:150px;height:30px;background:#9bc90d;top:12px;right:-48px;text-align:center;font-size:.8em;font-family:sans-serif;font-weight:700;color:#fff;line-height:35px;transform:rotate(45deg)}</style>
 </head>
 <body>
