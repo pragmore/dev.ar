367e23c82e9ccfc94320bf926cb1262ccf47b92a
Rodrigo “Albo” Arce <albo@pragmore.com>
2023-04-15 20:43:57 -0300

    Agregar footer

---
+++ views/footer.jsx
@@ -0,0 +1,8 @@
+<footer>
+  <span>Hecho con ❤️ por <a href="https://pragmore.com">Pragmore</a></span>
+  <span>
+    <a href="https://twitter.com/pragmore" target="_blank" aria-label="GitHub">
+      <i class="tw" aria-hidden="true" title="Twitter"></i>
+    </a>
+  </span>
+</footer>
