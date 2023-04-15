d72c3afafec9e42a937b7a9e2b6af0c17ae83150
Rodrigo “Albo” Arce <albo@pragmore.com>
2023-04-15 20:44:57 -0300

    Agregar CDN de blouse.css

--- views/layout.jsx
+++ views/layout.jsx
@@ -1,9 +1,17 @@
+import Footer from './footer'
+import BetaRibbon from './betaRibbon'
+
 <html lang="es">
 <head>
   <meta charset="UTF-8"/>
   <title>{title}</title>
+  <link rel="stylesheet" href="https://cdn.pragmore.com/2.0.6/blouse.css?font=Poppins&family=sans-serif" />
+  <BetaRibbon />
 </head>
 <body>
-  {children}
+  <main>
+    {children}
+  </main>
+  <Footer />
 </body>
 </html>
