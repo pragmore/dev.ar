d2b7547eabae907348c11a7f0e748a197f603fb0
Rodrigo “Albo” Arce <albo@pragmore.com>
2023-04-22 22:42:41 -0300

    Agregar configuracion de dotenv para la base

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import { Sequelize, DataTypes } from 'sequelize'
 
-// TODO: Configurar con el env
-export const db = new Sequelize('sqlite::memory:', {define: {underscored: true}})
+const URI = `${process.env.DB_CONNECTION}://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+export const db = new Sequelize(URI, {define: {underscored: true}})
 export const Domain = db.define('Domain', {
   fqdn: DataTypes.STRING,
   redirect: DataTypes.STRING,
