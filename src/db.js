61738727ccbc13b04ce5d2795fdfcbeaa2351d93
Rodrigo “Albo” Arce <albo@pragmore.com>
2023-04-24 01:48:55 -0300

    Agregar comentarios

--- src/db.js
+++ src/db.js
@@ -1,10 +1,13 @@
 import { Sequelize, DataTypes } from 'sequelize'
 
+// Conexión
 const URI = `${process.env.DB_CONNECTION}://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 export const db = new Sequelize(URI.replace(/:\/$/, ''), {define: {
   underscored: true,
   charset: 'utf8',
 }})
+
+// Modelos
 export const Domain = db.define('Domain', {
   fqdn: DataTypes.STRING,
   redirect: DataTypes.STRING,
