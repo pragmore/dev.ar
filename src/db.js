8339cbd9e00a2af2596a72ce3e24ac98d7247995
Rodrigo “Albo” Arce <albo@pragmore.com>
2023-04-24 01:15:08 -0300

    Corregir nombre de base de datos local

--- src/db.js
+++ src/db.js
@@ -1,7 +1,10 @@
 import { Sequelize, DataTypes } from 'sequelize'
 
 const URI = `${process.env.DB_CONNECTION}://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-export const db = new Sequelize(URI, {define: {underscored: true}})
+export const db = new Sequelize(URI.replace(/:\/$/, ''), {define: {
+  underscored: true,
+  charset: 'utf8',
+}})
 export const Domain = db.define('Domain', {
   fqdn: DataTypes.STRING,
   redirect: DataTypes.STRING,
