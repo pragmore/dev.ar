dc9e409909279b3d6aad09681b9ad1594e4e5ada
Rodrigo “Albo” Arce <albo@pragmore.com>
2023-04-28 09:34:24 -0300

    Redirigir dominios que empiezan con www

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@ import express from 'express'
 import engine from 'express-engine-jsx'
 import {db, Domain} from './db.js'
 
+const START_WWW = /^www\./
 const app = express()
 
 app.set('view engine', 'jsx')
 app.engine('jsx', engine)
 
 app.use(async (req, res, next) => {
+  if (req.hostname.match(START_WWW)) {
+    const host = req.get('host').replace(START_WWW, '')
+    const url = req.protocol + '://' + host + req.originalUrl
+    return res.redirect(url)
+  }
   const foundedDomain = await Domain.findOne({where: {fqdn: req.hostname}})
   if (foundedDomain?.redirect) {
     res.redirect(foundedDomain.redirect)
