e29140f3e4e52160ddf99c0a06c7c7c94baafa6a
Rodrigo “Albo” Arce <albo@pragmore.com>
2023-05-04 10:48:16 -0300

    Cambiar tamaño de la fuente

--- views/layout.jsx
+++ views/layout.jsx
@@ -7,7 +7,7 @@ import BetaRibbon from './betaRibbon'
   <title>{title}</title>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="icon" href="data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20viewBox=%220%200%20100%20100%22%3E%3Ctext%20y=%22.9em%22%20font-size=%2290%22%3E👩‍💻%3C/text%3E%3C/svg%3E" />
-  <link rel="stylesheet" href="https://cdn.pragmore.com/2.0.7/blouse.css?font=Poppins&family=sans-serif&size=13pt" />
+  <link rel="stylesheet" href="https://cdn.pragmore.com/2.0.7/blouse.css?font=Poppins&family=sans-serif" />
   <BetaRibbon />
 </head>
 <body>
